cbc42f1fe730515a1d49581a46dd8018d087cc71
0shanAbry1 <0shanAbry1@users.noreply.github.com>
2015-10-05 00:51:00 -0100

    Update maze.js
    Cheater functionality added.

--- maze.js
+++ maze.js
@@ -1,10 +1,49 @@
 var flag = false;
+var istart = false;
 
 window.onload = function() {
     	//Exercise 1:- $("boundary1").onmouseover = overLine;
     
 	$("start").onclick = startGame; //To begin the game
-    	$("end").onmouseover = endGame; //To end the game	
+    	$("end").onmouseover = endGame; //To end the game
+
+	//$("body:not(div#maze)").onmouseover = cheatGame;
+	//$("body").children().not("#maze").onmouseover = cheatGame;
+	//$(document.body).contents().not('#maze').onmouseover = cheatGame;
+	//$("body *").not("#maze").onmouseover = cheatGame;
+
+	/**/
+	$(document.body).onmousemove = function(e) {
+  		//Prints out current mouse position (Co-ordinates in browser window)
+		console.log('mouse X coord: ' + e.clientX);
+  		console.log('mouse Y coord: ' + e.clientY);
+
+  		//The maze dimensions
+  		var mz = $$("div#maze");
+  		var mzW = mz[0].offsetWidth;
+  		var mzH = mz[0].offsetHeight;
+  		var mzSX = mz[0].offsetLeft;
+  		var mzSY = mz[0].offsetTop;
+
+  		//Calculate the end points of the maze div
+  		var mzEX = parseInt(mzSX) + parseInt(mzW);
+  		var mzEY = parseInt(mzSY) + parseInt(mzH);
+
+  		console.log('Maze start X coord: ' + mzSX);
+  		console.log('Maze start Y coord: ' + mzSY);
+
+  		console.log('Maze end X coord: ' + mzEX);
+  		console.log('Maze end Y coord: ' + mzEY);
+
+  		// Cheating defined only if the player has started the game
+  		if (istart){
+      			// check if the mouse pointer is outside the maze
+      			if (e.clientX < mzSX || e.clientY < mzSY 
+			||e.clientY > mzEY || e.clientX > mzEX) {
+          			cheatGame();
+      			}
+  		}
+	}
 
 	/*
 	Exercise 2:- Array list >> Contents: Div containers [Id maze; Class boundary]
@@ -15,7 +54,7 @@ window.onload = function() {
 	for(var k = 0; k < borders.length; k++){
 		borders[k].onmouseover = overLine;
 	}
-};
+}
 
 function overLine() {
 	//Exercise 1:- $("boundary1").addClassName("youlose");
@@ -25,18 +64,23 @@ function overLine() {
 	Response >> Turns the background of all elements to Red
 	*/
 
+	flag = true; //Player loses the game
+	istart = false; 
+
+	$("status").textContent = "You lose! :(";
+
 	var borders = $$("div#maze div.boundary");
 	for(var k = 0; k < borders.length; k++){
 		borders[k].addClassName("youlose");
 	}
-
-	flag = true; //Player loses the game
-	$("status").textContent = "You lose! :(";
 }
 
 function startGame(){
 	flag = false;
-	
+	istart = true;	
+
+	$("status").textContent = "Let's do this! :D Find the E";
+
 	/*
 	Exercise 4:- Array list >> Contents: Div containers [Id maze; Class boundary]
 	Response >> Resets background colour
@@ -45,8 +89,14 @@ function startGame(){
 	var borders = $$("div#maze div.boundary");
 	for (var i = 0; i < borders.length; i++) {
         	borders[i].removeClassName("youlose");}
+}
 
-	$("status").textContent = "Let's do this! :D Find the E";
+function cheatGame(){
+	if(istart){
+		overLine();
+		$("status").textContent = "You cheater!!! >_<";
+		
+	}	
 }
 
 function endGame(){
